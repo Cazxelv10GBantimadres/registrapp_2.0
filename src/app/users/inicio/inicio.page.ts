4b8b4c1e0d44d741f94a23531f3febe7ae5bba42
Cazxelv10GBantimadres <133673965+Cazxelv10GBantimadres@users.noreply.github.com>
2024-12-05 20:07:38 -0300

    recuperacion

--- src/app/users/inicio/inicio.page.ts
+++ src/app/users/inicio/inicio.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -6,22 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  @ViewChild('subjectBox', { read: ElementRef }) subjectBox!: ElementRef;
 
-  constructor() { }
+  constructor(private router: Router, private animationCtrl: AnimationController) {}
 
   ngOnInit() {
+    // Llama a animateSubjectBox después de que la vista se haya inicializado
+    setTimeout(() => {
+      this.animateSubjectBox();
+    }, 1000); // Espera 1 segundo antes de animar el elemento
   }
 
   scanQRCode() {
-    // Lógica para escanear código QR
     console.log('Escanear código QR');
-    // Aquí puedes agregar la lógica para escanear el código QR
+    // Lógica para escanear el código QR
   }
 
   generateQRCode() {
-    // Lógica para generar código QR
     console.log('Generar código QR');
-    // Aquí puedes agregar la lógica para generar el código QR
+    // Lógica para generar el código QR
   }
 
+  goToLogin() {
+    this.router.navigate(['/register']); 
+  }
+
+ animateSubjectBox() {
+  const animation: Animation = this.animationCtrl.create()
+    .addElement(this.subjectBox.nativeElement)
+    .duration(0) // Establece la duración a 0 para que no haya animación
+    .fromTo('transform', 'translateX(0)', 'translateX(0)'); // Mantén la transformación en la posición original
+    
+  animation.play();
+}
 }
