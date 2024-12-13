bbab41a2443dab439c67e529f2268f5b0b873722
Cazxelv10GBantimadres <133673965+Cazxelv10GBantimadres@users.noreply.github.com>
2024-12-12 23:25:04 -0300

    edicion al boton "generar codigo qr"
    reduccion de tamaño y adaptacion

--- src/app/generate-qr/generate-qr.page.ts
+++ src/app/generate-qr/generate-qr.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-generate-qr',
@@ -9,8 +10,15 @@ export class GenerateQRPage {
   isQRCodeVisible: boolean = false; // Controla si el QR es visible
   qrCodeImage: string = 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Commons_QR_code.png'; // Imagen QR predeterminada
 
+  constructor(private router: Router) {}
+
   // Genera y muestra el código QR
   generateQRCode() {
     this.isQRCodeVisible = true;
+  }
+
+  // Navegar hacia atrás
+  goBack() {
+    this.router.navigate(['/inicio']); // Reemplaza 'inicio' con la ruta de tu página de inicio
   }
 }
